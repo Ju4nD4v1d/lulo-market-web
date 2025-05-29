a728e320cf6fcf40b0882eb444e9ed5d7c361e13
Juan David Ortega <32989874+Ju4nD4v1d@users.noreply.github.com>
2025-05-29 14:56:35 -0700

    Update form styles to use green primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,5 +26,34 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addBase, theme }) {
+      addBase({
+        // Override default focus styles
+        '[type="text"]:focus, [type="email"]:focus, [type="url"]:focus, [type="password"]:focus, [type="number"]:focus, [type="date"]:focus, [type="datetime-local"]:focus, [type="month"]:focus, [type="search"]:focus, [type="tel"]:focus, [type="time"]:focus, [type="week"]:focus, [multiple]:focus, textarea:focus, select:focus': {
+          '--tw-ring-color': theme('colors.primary.500'),
+          '--tw-ring-offset-shadow': 'var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color)',
+          '--tw-ring-shadow': 'var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color)',
+          'box-shadow': 'var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000)',
+          'border-color': theme('colors.primary.500'),
+        },
+        // Override checkbox and radio styles
+        '[type="checkbox"]:checked, [type="radio"]:checked': {
+          'background-color': theme('colors.primary.500'),
+          'border-color': theme('colors.primary.500'),
+        },
+        '[type="checkbox"]:focus, [type="radio"]:focus': {
+          '--tw-ring-color': theme('colors.primary.500'),
+        },
+        // Override time input styles
+        '[type="time"]': {
+          'color-scheme': 'light',
+        },
+        '[type="time"]:focus': {
+          '--tw-ring-color': theme('colors.primary.500'),
+          'border-color': theme('colors.primary.500'),
+        },
+      });
+    },
+  ],
 };
